0a2219c7d93b1e92d9dbd8bd4976f9625b207981
Daniel Giao <daniel.giao@icloud.com>
2024-08-16 18:13:18 -0400

    invApp views attribute bug fixed

--- invProject/invApp/views.py
+++ invProject/invApp/views.py
@@ -23,21 +23,21 @@ def product_list_view(request):
 
 # Update View
 def product_update_view(request, product_id):
-    product = Product.objects().get(product_id=product_id)
-    form = ProductForm(instance=product)
-    if request.method == 'PUT':
-        form = ProductForm(request.PUT, instance=product)
+    product = Product.objects.get(product_id=product_id)
+    form = ProductForm()
+    if request.method == "POST":
+        form = ProductForm(request.POST,instance=product)
         if form.is_valid():
             form.save()
-            return redirect('product_list)')
-    return render(request, 'invApp/product_form.html', {'form': form})
+            return redirect('product_list')
+    return render(request, 'invApp/product_form.html', {'form':form})
 
 # Delete View
 def product_delete_view(request, product_id):
-    product = Product.objects().get(product_id=product_id)
+    product = Product.objects.get(product_id=product_id)
     form = ProductForm(instance=product)
-    if request.method == 'DELETE':
-        form = ProductForm(request.DELETE, instance=product)
+    if request.method == "POST":
+        form = ProductForm(request.POST, instance=product)
         if form.is_valid():
             form.delete()
             return redirect('product_list)')
