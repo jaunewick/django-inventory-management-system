cc0830b7abb892544733ca61f31248bbe9a04ab7
Daniel Giao <daniel.giao@icloud.com>
2024-08-16 18:17:56 -0400

    Delete bug fixed

--- invProject/invApp/views.py
+++ invProject/invApp/views.py
@@ -35,10 +35,7 @@ def product_update_view(request, product_id):
 # Delete View
 def product_delete_view(request, product_id):
     product = Product.objects.get(product_id=product_id)
-    form = ProductForm(instance=product)
     if request.method == "POST":
-        form = ProductForm(request.POST, instance=product)
-        if form.is_valid():
-            form.delete()
-            return redirect('product_list)')
+        product.delete()
+        return redirect('product_list')
     return render(request, 'invApp/product_confirm_delete.html', {'product': product})
